9c1ff59c340e1160c2872caacea7d9982b6461eb
jatinar997 <jatin.arora@nubiz.in>
2025-10-17 00:53:59 +0530

    ui

--- src/app/(nonauth)/home/page.tsx
+++ src/app/(nonauth)/home/page.tsx
@@ -1,17 +1,18 @@
 // components
 import { Navbar, Footer } from "@/components";
 
-// sections
-import Hero from "@/components/pages-utilities/hero";
 import ExploreCourses from "@/components/pages-utilities/explore-courses";
 import React from "react";
+import Carousel from "@/components/pages-utilities/hero-carousel";
 
 function Campaign() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <ExploreCourses />
+      <Carousel />
+      <div>
+        <ExploreCourses />
+      </div>
       <Footer />
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 // components
 import { Navbar, Footer } from "@/components";
 
-// sections
-import Hero from "@/components/pages-utilities/hero";
 import ExploreCourses from "@/components/pages-utilities/explore-courses";
 import React from "react";
 import Carousel from "@/components/pages-utilities/hero-carousel";
@@ -11,7 +9,6 @@ function Campaign() {
   return (
     <>
       <Navbar />
-      {/* <Hero /> */}
       <Carousel />
       <div>
         <ExploreCourses />
