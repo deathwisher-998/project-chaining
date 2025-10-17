a71e7c0f77b9ed3f79a1eb70524a3100912b937f
jatinar997 <jatin.arora@nubiz.in>
2025-10-17 10:07:49 +0530

    ui

--- src/components/profile/orderList.tsx
+++ src/components/profile/orderList.tsx
@@ -17,57 +17,87 @@ export default function Orderlists({ odrerlisting }: { odrerlisting: any }) {
     setOpen(open === value ? 0 : value);
   };
 
-  return (
-   <>
-    {odrerlisting?.length > 0 ? <div className="w-full mx-auto">
-      {odrerlisting.map((order: any) => (
-        <Accordion key={order.id} open={open === order.id}>
-          {/* Accordion Header */}
-          <AccordionHeader
-            onClick={() => handleOpen(order.id)}
-            className={`flex justify-between items-center gap-2 px-4 rounded-md ${open === order.id && 'shadow-md'}`}
-            style={{ backgroundColor: "#c5c5c5", borderBottomColor: "#fff" }}
-          >
-            <div className="w-full grid grid-cols-2">
-              <div>
-                <span className="font-semibold">
-                  {"Order-" + order.id.slice(0, 7)}
-                </span>
-              </div>
-              <div className="text-right">
-                <span
-                  className={`text-sm font-bold ${
-                    order.status === "Delivered" ? "text-gray" : "text-blue"
-                  }`}
-                >
-                  {order.status}
-                </span>
-              </div>
-            </div>
-          </AccordionHeader>
+  function orderDate(date: string) {
+    return "";
+  }
 
-          {/* Accordion Body */}
-          <AccordionBody>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-              {order.orderItems.map((item, index) => (
-                <div
-                  key={index}
-                  className="border rounded-lg p-3 shadow-sm bg-white flex flex-col"
-                >
-                  <span className="font-semibold text-black">{item.product.name}</span>
-                  <span className="text-sm text-gray-800">
-                    Qty: {item.quantity}
-                  </span>
-                  <span className="font-semibold">{item.price}</span>
+  return (
+    <>
+      {odrerlisting?.length > 0 ? (
+        <div className="w-full mx-auto">
+          {odrerlisting.map((order: any) => (
+            <Accordion key={order.id} open={open === order.id}>
+              {/* Accordion Header */}
+              <AccordionHeader
+                onClick={() => handleOpen(order.id)}
+                className={`flex justify-between items-center gap-2 px-4 rounded-md mb-2 ${
+                  open === order.id && "shadow-md"
+                }`}
+                style={{
+                  backgroundColor: "#77f6d6",
+                  borderBottomColor: "#fff",
+                }}
+              >
+                <div className="w-full grid grid-cols-2">
+                  <div>
+                    <span className="font-semibold">
+                      {/* {"Order On" + orderDate(order?.orderItems[0]?.createdOn)} */}
+                      {"Order On"}
+                    </span>
+                  </div>
+                  <div className="text-right">
+                    <span
+                      className={`text-sm font-bold ${
+                        order.status === "Delivered" ? "text-gray" : "text-blue"
+                      }`}
+                    >
+                      {order.status}
+                    </span>
+                  </div>
                 </div>
-              ))}
-            </div>
-          </AccordionBody>
-        </Accordion>
-      ))}
-    </div> : <div className="text-center mt-10 mb-10">
-       <h1 className="text-xl font-semibold text-color-by-logo-2">No Order Found</h1>
-    </div> }
-   </>
+              </AccordionHeader>
+
+              {/* Accordion Body */}
+              <AccordionBody>
+                <div className="bg-gray-800 rounded-md mb-3 px-4 py-2">
+                  <div>
+                    <p className="font-semibold text-white">
+                      Shipping Address : {order?.address?.addressLin1 + ", " + `${order?.address?.addressLin2 ? order?.address?.addressLin2 + ", " : ""}` + order?.address?.postalCode + ", " + order?.address?.country + ", " + order?.address?.city}
+                    </p>
+                    <p className="font-semibold text-white">Contact No: {order?.address?.phoneNumber}</p>
+                  </div>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                  {order.orderItems.map((item:any, index:number) => (
+                    <div
+                      key={index}
+                      className="border rounded-lg p-3 shadow-sm bg-white flex flex-col"
+                    >
+                      <div>
+                        <p className="font-semibold text-black">
+                          {item.product.name}
+                        </p>
+                        <p className="text-sm text-gray-800">
+                          Quantity: {item.quantity}
+                        </p>
+                        <span className="font-semibold">
+                          Total Amount : {item.price} Rs
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </AccordionBody>
+            </Accordion>
+          ))}
+        </div>
+      ) : (
+        <div className="text-center mt-10 mb-10">
+          <h1 className="text-xl font-semibold text-color-by-logo-2">
+            No Order Found
+          </h1>
+        </div>
+      )}
+    </>
   );
 }
